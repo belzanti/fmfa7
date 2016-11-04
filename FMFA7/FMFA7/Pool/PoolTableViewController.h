ef61dc1fce1ed72642f4dae8875f174e396480f6
rbelzanti <remi.belzanti@gmail.com>
2016-11-04 01:57:59 +0100

    - [NEW] Pool view controller

---
+++ FMFA7/FMFA7/Pool/PoolTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  PoolTableViewController.h
+//  FMFA7
+//
+//  Created by Web Libre Consulting on 04/11/2016.
+//  Copyright © 2016 Belzanti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PoolTableViewController : UITableViewController
+
+@property (weak, nonatomic) IBOutlet UITableView *tableview;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *searchBarButtonItem;
+
+@end
